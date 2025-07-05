c11d87675e35f51fdd696935ada63c7fb7802b32
Mosleh92 <121885983+Mosleh92@users.noreply.github.com>
2025-07-06 00:21:11 +0400

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,4 @@
 import { Suspense, lazy } from 'react';
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://qimzwedwkxhhemmxgciv.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFpbXp3ZWR3a3hoaGVtbXhnY2l2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA1OTk1MjYsImV4cCI6MjA2NjE3NTUyNn0.VhoRtF1BXlrOPT929_fwN__UUwCSpGPM7G-Lk73X1_I';
-export const supabase = createClient(supabaseUrl, supabaseKey);
 import { Toaster } from 'react-hot-toast';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Loading from './components/UI/Loading';
