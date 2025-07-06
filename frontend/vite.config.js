d549e3170a87cabba1363ff06d3d4c1c09711c13
Mosleh92 <121885983+Mosleh92@users.noreply.github.com>
2025-07-06 18:21:19 +0400

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,13 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import path from 'path-browserify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(new URL('./src', import.meta.url).pathname),
     },
   },
   server: {
