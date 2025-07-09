ddd17b2413c86fefb772b013b33d308299a56737
Mosleh92 <121885983+Mosleh92@users.noreply.github.com>
2025-07-09 19:02:02 +0400

    Update User.js

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -7,25 +7,12 @@ const userSchema = new mongoose.Schema({
     required: true,
     unique: true,
     lowercase: true,
-    trim: true,
-    match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 'Please enter a valid email']
+    trim: true
   },
   password: {
     type: String,
     required: true,
-    minlength: 8
-  },
-  firstName: {
-    type: String,
-    required: true,
-    trim: true,
-    maxlength: 50
-  },
-  lastName: {
-    type: String,
-    required: true,
-    trim: true,
-    maxlength: 50
+    minlength: 6
   },
   role: {
     type: String,
@@ -34,123 +21,49 @@ const userSchema = new mongoose.Schema({
   },
   tenantId: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Tenant',
-    required: function() {
-      return this.role !== 'super_admin';
-    }
+    ref: 'Tenant'
+  },
+  subTenantId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'SubTenant'
+  },
+  profile: {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    phone: String,
+    nationalId: String,
+    address: String,
+    dateOfBirth: Date,
+    avatar: String
   },
   isActive: {
     type: Boolean,
     default: true
   },
-  isVerified: {
+  isEmailVerified: {
     type: Boolean,
     default: false
   },
-  verificationToken: String,
-  resetPasswordToken: String,
-  resetPasswordExpires: Date,
   lastLogin: Date,
   loginAttempts: {
     type: Number,
     default: 0
   },
-  lockUntil: Date,
-  twoFactorEnabled: {
-    type: Boolean,
-    default: false
-  },
-  twoFactorSecret: String,
-  profilePicture: String,
-  phoneNumber: String,
-  address: {
-    street: String,
-    city: String,
-    state: String,
-    zipCode: String,
-    country: String
-  },
-  preferences: {
-    language: {
-      type: String,
-      default: 'en'
-    },
-    timezone: {
-      type: String,
-      default: 'UTC'
-    },
-    notifications: {
-      email: {
-        type: Boolean,
-        default: true
-      },
-      sms: {
-        type: Boolean,
-        default: false
-      },
-      push: {
-        type: Boolean,
-        default: true
-      }
-    }
-  }
+  lockUntil: Date
 }, {
-  timestamps: true,
-  toJSON: {
-    transform: function(doc, ret) {
-      delete ret.password;
-      delete ret.verificationToken;
-      delete ret.resetPasswordToken;
-      delete ret.twoFactorSecret;
-      return ret;
-    }
-  }
-});
-
-// Indexes for performance
-userSchema.index({ email: 1 });
-userSchema.index({ tenantId: 1, role: 1 });
-userSchema.index({ isActive: 1 });
-
-// Virtual for account lock status
-userSchema.virtual('isLocked').get(function() {
-  return !!(this.lockUntil && this.lockUntil > Date.now());
+  timestamps: true
 });
 
 // Hash password before saving
 userSchema.pre('save', async function(next) {
   if (!this.isModified('password')) return next();
-  
-  try {
-    const salt = await bcrypt.genSalt(12);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (error) {
-    next(error);
-  }
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
 });
 
 // Compare password method
 userSchema.methods.comparePassword = async function(candidatePassword) {
-  return await bcrypt.compare(candidatePassword, this.password);
-};
-
-// Increment login attempts
-userSchema.methods.incLoginAttempts = function() {
-  if (this.lockUntil && this.lockUntil < Date.now()) {
-    return this.updateOne({
-      $unset: { lockUntil: 1 },
-      $set: { loginAttempts: 1 }
-    });
-  }
-  
-  const updates = { $inc: { loginAttempts: 1 } };
-  
-  if (this.loginAttempts + 1 >= 5 && !this.isLocked) {
-    updates.$set = { lockUntil: Date.now() + 2 * 60 * 60 * 1000 }; // 2 hours
-  }
-  
-  return this.updateOne(updates);
+  return bcrypt.compare(candidatePassword, this.password);
 };
 
 module.exports = mongoose.model('User', userSchema);
