d9d0404faeb5059df0c077bb7d92492393589aca
Mosleh92 <121885983+Mosleh92@users.noreply.github.com>
2025-07-09 19:04:56 +0400

    Update User.js

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,3 +1,4 @@
+// backend/src/models/User.js
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
@@ -6,8 +7,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true,
-    lowercase: true,
-    trim: true
+    lowercase: true
   },
   password: {
     type: String,
@@ -23,47 +23,26 @@ const userSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Tenant'
   },
-  subTenantId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'SubTenant'
-  },
   profile: {
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
+    firstName: String,
+    lastName: String,
     phone: String,
-    nationalId: String,
-    address: String,
-    dateOfBirth: Date,
     avatar: String
   },
   isActive: {
     type: Boolean,
     default: true
-  },
-  isEmailVerified: {
-    type: Boolean,
-    default: false
-  },
-  lastLogin: Date,
-  loginAttempts: {
-    type: Number,
-    default: 0
-  },
-  lockUntil: Date
-}, {
-  timestamps: true
-});
+  }
+}, { timestamps: true });
 
-// Hash password before saving
 userSchema.pre('save', async function(next) {
   if (!this.isModified('password')) return next();
   this.password = await bcrypt.hash(this.password, 12);
   next();
 });
 
-// Compare password method
-userSchema.methods.comparePassword = async function(candidatePassword) {
-  return bcrypt.compare(candidatePassword, this.password);
+userSchema.methods.comparePassword = async function(password) {
+  return bcrypt.compare(password, this.password);
 };
 
 module.exports = mongoose.model('User', userSchema);
